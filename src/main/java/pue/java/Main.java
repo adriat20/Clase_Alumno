30cc5af69de41efb6f56b0bcc53dfa4d6c482b97
adriat20 <Adrianat17@educastur.es>
2024-02-22 14:06:41 +0100

    Codigo mejorado. Añadimos el resultado de las notas y aprobados/suspensos, distinguiendo por sexos. También ahora se crean dos listas de estudiantes en lugar de reutilizar una.

--- src/main/java/pue/java/Main.java
+++ src/main/java/pue/java/Main.java
@@ -12,9 +12,8 @@ public class Main {
 
 
         List<Aula> aulas = new ArrayList<>();
-        aulas.add(new Aula(1, materiaProfesorDisponible, estudiantes, profesor));
-        //Creo un nuevo aula con la misma lista para no usar más memoria ya que la lógica es para que salga la excepcion
-        aulas.add(new Aula(2, materiaProfesorAusente, estudiantes, profesor));
+        aulas.add(new Aula(1, materiaProfesorDisponible,  Estudiante.generaEstudiantes(), profesor));
+        aulas.add(new Aula(2, materiaProfesorAusente,     Estudiante.generaEstudiantes(), profesor));
 
 
         // Se recorren todas las aulas disponibles
@@ -32,44 +31,76 @@ public class Main {
                            .sorted(Comparator.comparing(Estudiante::getApellido))
                            .forEach(System.out::println);
 
-                System.out.println("\nCantidad de alumnos aprobados: " + calcularAprobados(estudiantesAula) + "\n" +
-                        "Cantidad de alumnos suspensos: " + calcularSuspensos(estudiantesAula) + "\n" +
-                        "Nota media de la clase: " + calcularMediaNotas(estudiantesAula) + "\n");
+                System.out.println("\nCantidad de alumnos aprobados: " + calcularAprobados(estudiantesAula)[0] + "\n" +
+                                   "Cantidad de alumnas aprobadas: " + calcularAprobados(estudiantesAula)[1]   + "\n"+
+                                   "\nCantidad de alumnos suspensos: "   +  calcularSuspensos(estudiantesAula)[0] + "\n" +
+                                   "Cantidad de alumnas suspensas: "   +  calcularSuspensos(estudiantesAula)[1] + "\n" +
+                        "\nNota media de la clase de los alumnos: " + calcularMediaNotas(estudiantesAula)[0] + "\n"+
+                        "Nota media de la clase de las alumnas: " + calcularMediaNotas(estudiantesAula)[1] + "\n"+
+                        "\nNota media total de la clase: "          + calcularMediaNotas(estudiantesAula)[2] + "\n");
             }
         }
     }
 
-    public static int calcularAprobados(List<Estudiante> estudiantes) {
-        int contador = 0;
+    public static int[] calcularAprobados(List<Estudiante> estudiantes) {
+        int contadorM = 0;
+        int contadorF = 0;
+
         for (Estudiante estudiante : estudiantes) {
             if (estudiante.getCalificacion() >= 5) {
-                contador++;
+                if (estudiante.getSexo() == 'M') {
+                    contadorM++;
+                } else if (estudiante.getSexo() == 'F') {
+                    contadorF++;
+                }
             }
         }
-        return contador;
+        return new int[] {contadorM, contadorF};
     }
 
-    public static int calcularSuspensos(List<Estudiante> estudiantes) {
-        int contador = 0;
+
+    public static int[] calcularSuspensos(List<Estudiante> estudiantes) {
+        int contadorM = 0;
+        int contadorF = 0;
+
         for (Estudiante estudiante : estudiantes) {
             if (estudiante.getCalificacion() < 5) {
-                contador++;
+                if (estudiante.getSexo() == 'M') {
+                    contadorM++;
+                } else if (estudiante.getSexo() == 'F') {
+                    contadorF++;
+                }
             }
         }
-        return contador;
+        return new int[] {contadorM, contadorF};
     }
 
-    public static float calcularMediaNotas(List<Estudiante> estudiantes) {
-        float media, acumulador = 0;
-        int contador = 0;
+
+    public static float [] calcularMediaNotas(List<Estudiante> estudiantes) {
+        float mediaTotal, mediaM, mediaF, acumuladorTotal = 0, acumuladorM = 0, acumuladorF = 0;
+        int contadorTotal = 0, contadorM = 0, contadorF = 0;
+
 
         for (Estudiante estudiante : estudiantes) {
-            acumulador += estudiante.getCalificacion();
-            contador++;
-        }
+            if(estudiante.getSexo()=='M'){
+            acumuladorM += estudiante.getCalificacion();
+            contadorM++;
+        } else if (estudiante.getSexo()=='F') {
+                acumuladorF += estudiante.getCalificacion();
+                contadorF++;
+            }
+            contadorTotal ++;
+            acumuladorTotal += estudiante.getCalificacion();
+            }
 
-        media = acumulador / contador;
+            //mediaM = acumuladorM / contadorM;
+            mediaM     = contadorM     > 0 ? acumuladorM / contadorM :0;
 
-        return media;
+            mediaF     = contadorF     > 0 ? acumuladorF / contadorF :0;
+
+            mediaTotal = contadorTotal > 0 ? acumuladorTotal / contadorTotal : 0;
+
+
+        return new float [] {mediaM,mediaF,mediaTotal} ;
     }
 }
